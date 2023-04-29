25787ec2383402296c4dfc3a4b7bad35b94c2a36
venz0618 <cs8.carnavenivici@gmail.com>
2023-04-29 17:05:08 +0800

    oh yeah

--- front-end/src/components/DefaultLayout.jsx
+++ front-end/src/components/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { NavLink, Outlet } from 'react-router-dom'
+import { NavLink, Navigate, Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider'
 
 
@@ -19,7 +19,11 @@ function classNames(...classes) {
 
 export default function DefaultLayout() {
   
-  const {currentUser} = useStateContext();
+  const {currentUser, userToken} = useStateContext();
+
+  if (!userToken){
+    return <Navigate to='login'/>
+  }
 
   const logout = (ev) => {
     ev.preventDefault();
--- front-end/src/views/Surveys.jsx
+++ front-end/src/views/Surveys.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import PageComponents from '../components/PageComponents'
+import { useStateContext } from '../contexts/ContextProvider'
+import SurveyListItem from '../components/SurveyListItem';
 
 function Surveys() {
+  const {surveys} = useStateContext();
+  console.log(surveys);
+
+  const onDeleteClick = () => {
+    console.log('On Delete Click')
+  }
   return (
     <PageComponents title="Surveys">
-    Surveys Content
+
+      <div className='grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3'>
+          {surveys.map((survey) =>(
+            <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick}/>
+          ))}
+      </div>
+       
    </PageComponents>
   )
 }
